ef7afd45e2b60553ff121caeb04db625288c9565
Rajahni fields <rajahnifields@gmail.com>
2021-04-06 00:51:33 -0400

    Got navbar working, working on portfolio

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 // import AboutMe from "./components/Aboutme";
 import FooterPage from "./components/Footer";
 import Home from "./components/Home";
 import NavbarPage from "./components/Navbar";
-// import Portfolio from "./components/Portfoilio";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Portfolio from "./components/Portfolio";
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-      <FooterPage />
-      <NavbarPage />
-      {/* <AboutMe />
-      <Portfolio /> */}
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <Home /> */}
+
+//       <NavbarPage />
+//       {/* <AboutMe />
+//       <Portfolio /> */}
+//       <FooterPage />
+//     </div>
+//   );
+// }
+class App extends Component {
+  state = {
+    Portfolio,
+    FooterPage,
+    Home,
+    NavbarPage,
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <NavbarPage />
+        {/* <Home /> */}
+        <Title>My Portfolio</Title>
+        {this.Portfolio.state.map((Project) => (
+          <Portfolio
+            id={Project.id}
+            key={Project.id}
+            name={Project.name}
+            image={Project.image}
+            occupation={Project.occupation}
+            location={Project.location}
+          />
+        ))}
+        <FooterPage />
+      </Wrapper>
+    );
+  }
 }
-
 export default App;
--- react-portfolio/src/components/Home.js
+++ react-portfolio/src/components/Home.js
@@ -11,33 +11,11 @@ import React from "react";
 
 function Home() {
   return (
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol>
-          <MDBJumbotron style={{ padding: 0 }}>
-            <MDBCol
-              className="text-white text-center py-5 px-4 my-5"
-              style={{
-                backgroundImage: `url(https://mdbcdn.b-cdn.net/img/Photos/Others/gradient1.jpg)`,
-              }}
-            >
-              <MDBCol className="py-5">
-                <MDBCardTitle className="h1-responsive pt-3 m-5 font-bold">
-                  This is my awesome Portfolio!!
-                </MDBCardTitle>
-                <p className="mx-5 mb-5">
-                  Welcome to my portfolio, click on the differnet navbar
-                  elements to look through this experience.
-                </p>
-                <MDBBtn outline color="white" className="mb-5">
-                  <MDBIcon icon="clone" className="mr-2"></MDBIcon> View project
-                </MDBBtn>
-              </MDBCol>
-            </MDBCol>
-          </MDBJumbotron>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <div id="intro" class="bg-image shadow-2-strong">
+      <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);">
+        <div class="container d-flex align-items-center justify-content-center text-center h-100"></div>
+      </div>
+    </div>
   );
 }
 export default Home;
