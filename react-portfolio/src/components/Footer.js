dd9fad770cf73eccaa45aaa7aca2865216dcea75
Rajahni fields <rajahnifields@gmail.com>
2021-04-06 22:38:53 -0400

    added right link to projects

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -13,8 +13,7 @@ const FooterPage = () => {
       </MDBContainer>
       <div className="footer-copyright text-center py-3">
         <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright:{" "}
-          <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
+          &copy; {new Date().getFullYear()} Copyright:{"2021 "}
         </MDBContainer>
       </div>
     </MDBFooter>
